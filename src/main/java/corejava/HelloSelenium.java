8d5df9ef6030120b9b4f815d0dd403dc701360ee
ganesh.pirale <ganesh.pirale@gmail.com>
2023-05-16 17:55:09 +0530

    Deleted some code

--- src/main/java/corejava/HelloSelenium.java
+++ src/main/java/corejava/HelloSelenium.java
@@ -3,6 +3,6 @@ package corejava;
 public class HelloSelenium {
     public static void main(String[] args) {
         System.out.println("Hello Selenium");
-        System.out.println("Modified code");
+        System.out.println("Modified");
     }
 }
