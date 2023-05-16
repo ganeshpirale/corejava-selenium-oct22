313f82cc24b6e472314b58a1e0bff8834e7b2f09
ganesh.pirale <ganesh.pirale@gmail.com>
2023-05-16 17:23:22 +0530

    First commit

---
+++ src/main/java/corejava/HelloJava.java
@@ -0,0 +1,9 @@
+package corejava;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        System.out.println("First java program");
+        System.out.println("First java program");
+    }
+
+}
