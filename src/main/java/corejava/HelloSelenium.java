63dad7fa9179422c786750e76249e695b4dc8403
ganesh.pirale <ganesh.pirale@gmail.com>
2023-05-16 17:50:56 +0530

    Third commit

--- src/main/java/corejava/HelloSelenium.java
+++ src/main/java/corejava/HelloSelenium.java
@@ -3,5 +3,6 @@ package corejava;
 public class HelloSelenium {
     public static void main(String[] args) {
         System.out.println("Hello Selenium");
+        System.out.println("Modified code");
     }
 }
