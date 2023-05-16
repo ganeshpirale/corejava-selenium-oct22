95804216c43070c5d952bb13d5ae3db650e31fc2
ganesh.pirale <ganesh.pirale@gmail.com>
2023-05-16 17:28:07 +0530

    Second commit

--- src/main/java/corejava/HelloJava.java
+++ src/main/java/corejava/HelloJava.java
@@ -3,7 +3,8 @@ package corejava;
 public class HelloJava {
     public static void main(String[] args) {
         System.out.println("First java program");
-        System.out.println("First java program");
+        System.out.println("Second java program");
+        System.out.println("Third java program");
     }
 
 }
